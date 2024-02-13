3e418556fad3f66077baab1ebe76b3fe263cd034
EricNG01 <102510835+EricNG01@users.noreply.github.com>
2024-02-13 21:20:14 +1000

    Draft version

---
+++ src/APIs/Api.js
@@ -0,0 +1,122 @@
+import axios from 'axios'
+import md5 from "md5";
+import timestamp from "time-stamp";
+
+// public key
+const PUB_KEY = "92387a221913641bb3041cd50a94d64d"; // n10641343@qut.edu.au
+// const PUB_KEY = "ad2e7fb9c421432353fa52a53a6efa48"; // ericng502502502@gmail.com
+// const PUB_KEY = "de1ab129447465668469fa0101dfc6a8"; // ericng502@gmail.com
+
+// private key;
+const PRI_KEY = "39844d0d6aa0369233feeb891e86689985c4b308"; // n10641343@qut.edu.au
+// const PRI_KEY = "d3066f889752a4093ddb3dabf8932f6ad544e16a"; // ericng502502502@gmail.com
+// const PRI_KEY = "7b0ef986454e44f9f4bbeeedb56fdb32711d5f07"; // ericng502@gmail.com
+
+// base URL
+const URL = "http://gateway.marvel.com";
+
+// Image size
+const icon = "/standard_xlarge.jpg"
+
+/* 
+ * API call - /v1/public/characters
+ * Randomly pick 1 letter from 'a' to 'z' and search 20 characters with the letter 
+ * Display the result from cache instead of getting all the stuffs from API calls directly
+ * Data to retrieve and save:
+ *  - character ID
+ *  - character name
+ *  - Description
+ *  - Icon Image URL (use standard_large)
+ *  
+ * Store the result into a list with a format of:
+ * [{ 
+ *    id: character id
+ *    name: character name
+ *    description: character description
+ *    image: character icon url 
+ *    landscape_img: character image url
+ *  }, 
+ *  {}, 
+ *  {}, ...]
+ */
+
+export const GetCharacters = async () => {
+  /* Time stamp */
+  const ts = timestamp();
+  /* Hash - md5 digest of (time stamp + private key + public key) */
+  const hash = md5(ts+PRI_KEY+PUB_KEY);
+
+  let nameStartsWith = 97 + Math.floor(Math.random() * 25);
+  let tempCharacters = [];  
+  try {     
+    await axios.get(`${URL}/v1/public/characters?nameStartsWith=${String.fromCharCode(nameStartsWith)}&apikey=${PUB_KEY}&ts=${ts}&hash=${hash}`)
+    .then((res) => {
+        for (let i = 0; i < res.data.data.results.length; i++) {
+            tempCharacters.push({
+              id: res.data.data.results[i].id,
+              name: res.data.data.results[i].name,
+              description: res.data.data.results[i].description,
+              image: res.data.data.results[i].thumbnail.path + icon,
+              status: "200"
+            })
+        }
+    })
+  } catch(e) {
+    const status = e.code;
+    tempCharacters.push({status: status});
+  }
+  return tempCharacters;
+}
+
+export const Search = async (name) => {
+  /* Time stamp */
+  const ts = timestamp();
+  /* Hash - md5 digest of (time stamp + private key + public key) */
+  const hash = md5(ts+PRI_KEY+PUB_KEY);
+  
+  let tempCharacter = [];
+  try {
+    await axios.get(`${URL}/v1/public/characters?nameStartsWith=${name}&limit=100&apikey=${PUB_KEY}&ts=${ts}&hash=${hash}`)
+    .then((res) => {
+      for (let i = 0; i < res.data.data.results.length; i++) { 
+        tempCharacter.push({
+          id: res.data.data.results[i].id,
+          name: res.data.data.results[i].name,
+          description: res.data.data.results[i].description.length>0?res.data.data.results[i].description: "No description",
+          image: res.data.data.results[i].thumbnail.path + icon,
+          status: "200"
+        });
+      }
+    })
+    } catch(e) {
+      const status = e.code;
+      tempCharacter.push({status: status});
+    }
+    return tempCharacter;
+}
+
+export const GetCharacter = async (id) => {
+  /* Time stamp */
+  const ts = timestamp();
+  /* Hash - md5 digest of (time stamp + private key + public key) */
+  const hash = md5(ts+PRI_KEY+PUB_KEY);
+  
+  let tempCharacter = [];
+  try {
+    await axios.get(`${URL}/v1/public/characters/${id}?apikey=${PUB_KEY}&ts=${ts}&hash=${hash}`)
+    .then((res) => {
+      tempCharacter.push({
+        id: res.data.data.results[0].id,
+        name: res.data.data.results[0].name,
+        description: res.data.data.results[0].description.length>0?res.data.data.results[0].description: "No description",
+        image: res.data.data.results[0].thumbnail.path + icon,
+        status: "200"
+      });
+    })
+  } catch(e) {
+    const status = e.code;
+    tempCharacter.push({status: status});
+  }
+  // console.log(tempCharacter)
+  return tempCharacter;
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,285 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import {GetCharacter, GetCharacters, Search} from './APIs/Api.js'
+import './App.css';
+import searchLogo from './searchLogo.svg'
+import Back from './Back.svg'
+import img_not_found from './image_not_found.jpg'
+
+
+import React, { useState } from 'react';
+
+/*
+ * A customized component that is used to display the characters' image
+ */
+function CharacterImageComponent({initialImageUrl, id}) {
+  // State to hold the current image URL
+  const [imageUrl, setImageUrl] = useState(initialImageUrl);
+
+  // Handler for image load error
+  const handleError = () => {
+    // Image not found with the url retrieved from the API get request
+    setImageUrl(img_not_found); // Set to fallback URL on error
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <img src={imageUrl} alt={id} id={id} className="icon" onError={handleError}/>
   );
 }
 
-export default App;
+
+export default function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading2, setIsLoading2] = useState(false);
+  const [characters, setCharacters] = useState([]); 
+  const [keywords, setKeyWords] = useState("");
+  const [selected, setSelected] = useState([]);
+  const [confirmedGK, setConfirmedGK] = useState("");
+  const [confirmedST, setConfirmedST] = useState("");
+  const [confirmedMD, setConfirmedMD] = useState("");
+  const [confirmedDF, setConfirmedDF] = useState("");
+  const [confirmedAdd, setConfirmedAdd] = useState("");
+  const [confirmedGKName, setConfirmedGKName] = useState("");
+  const [confirmedSTName, setConfirmedSTName] = useState("");
+  const [confirmedMDName, setConfirmedMDName] = useState("");
+  const [confirmedDFName, setConfirmedDFName] = useState("");
+  const [confirmedAddName, setConfirmedAddName] = useState("");
+  const [characterId, setCharacterId] = useState(0);
+  const [characterName, setCharacterName] = useState("");
+  const [characterUrl, setCharacterUrl] = useState("");
+  const [position, setPosition] = useState('');
+
+  const [selectGK, setSelecteGK] = useState(false);
+  const [selectST, setSelecteST] = useState(false);
+  const [selectMD, setSelecteMD] = useState(false);
+  const [selectDF, setSelecteDF] = useState(false);
+  const [selectAdd, setSelecteAdd] = useState(false);
+
+
+
+  const OrganizeData = async (res) => {
+    var result = [];  // Each row of the table 
+    var element = [];// Elements of each row
+    for (var i = 0; i < res.length; i++) {
+      element.push(
+        <div key={i} className="characters">
+          <button type="button" className="characters-option" onClick={OpenDetail}>
+            <div className="characters-option-name" id={res[i].id}>{res[i].name}</div>
+            <div className="characters-option-image"><CharacterImageComponent initialImageUrl={res[i].image} id={res[i].id}/></div>
+          </button>
+        </div>);
+    if (i % 5 === 4||i === res.length - 1) {
+      result.push(<>{element}</>);
+      element = [];
+    }}
+    
+    return result
+  }
+  const FetchData = async () => {
+    try {
+      setIsLoading(true);
+      const res = await GetCharacters();
+      const result = await OrganizeData(res);
+      setCharacters(<>{result}</>);
+    } catch(e) {
+      console.log("Error: " + e.status)
+      setCharacters(<h1>Service not available at the moment</h1>);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  const OpenCharacterList = (event) => {
+    FetchData();
+    let pos;
+    // Check if the event target itself is the image
+    if (event.target.tagName === 'IMG' || event.target.tagName === 'BUTTON' ) {
+      pos = event.target.id;
+    }
+
+    if (pos === "GK") { setSelecteGK(true); }
+    else if (pos === "ST") { setSelecteST(true); }
+    else if (pos === "MD") { setSelecteMD(true); }
+    else if (pos === "DF") { setSelecteDF(true); }
+    else if (pos === "Add") { setSelecteAdd(true); }
+    document.getElementById("overlay").style.height = "100%";
+    document.getElementById("overlay").style.visibility = "visible";
+  }
+  const CloseCharacterList = () => {
+    document.getElementById("overlay").style.height = "0%";
+    document.getElementById("overlay").style.visibility = "hidden";
+    document.getElementById("keywords").value = "";
+    setSelecteGK(false);
+    setSelecteST(false);
+    setSelecteMD(false);
+    setSelecteDF(false);
+    setSelecteAdd(false);
+  }
+  const SearchCharacter = async () => {
+    try{
+      setIsLoading(true);
+      const res = keywords? await Search(keywords) : await GetCharacters();
+      const result = await OrganizeData(res);
+      if (result.length > 0) {
+        setCharacters(<>{result}</>);
+      } else {
+        setCharacters(<>No such character, Please try other keywords</>)
+      }
+      
+    } catch(e) {
+      console.log("Error: " + e.status)
+      setCharacters(<h1>Service not available at the moment</h1>);
+    } finally {
+      setIsLoading(false);
+    }
+
+  }
+
+  const OpenDetail = async (event) => {
+    document.getElementById("overlay-2").style.height = "100%";
+    document.getElementById("overlay-2").style.visibility = "visible";
+    let imageId;
+    try {
+      setIsLoading2(true); 
+      // Check if the event target itself is the image
+      if (event.target.tagName === 'IMG' || event.target.tagName === 'DIV' ) {
+        imageId = event.target.id;
+      } 
+      else {
+        // If the button itself was clicked, find the image child
+        const image = event.target.querySelector('img');
+        if (image) {
+          imageId = image.id;
+          setCharacterId(imageId);
+        }
+      }
+      const result = await GetCharacter(imageId);
+      console.log(result)
+      setSelected(
+      <>
+        <CharacterImageComponent initialImageUrl={result[0].image} id={position}/>
+        <div className='name'>{result[0].name}</div>
+        <div className='description'>{result[0].description}</div>
+      </>);
+      setCharacterName(result[0].name);
+      setCharacterUrl(result[0].image);
+
+    } catch(e) {
+      console.log('Error:', e);
+    }
+    setIsLoading2(false);
+
+  }
+  const CloseDetail = async (event) => {
+    document.getElementById("overlay-2").style.height = "0";
+    document.getElementById("overlay-2").style.visibility = "hidden";
+    try {
+      if (event.target.id === "confirm"){
+        if (selectGK) { 
+          console.log(characterUrl)
+          setConfirmedGK(characterUrl)
+          setConfirmedGKName(characterName)
+        }
+        else if (selectST) { 
+          setConfirmedST(characterUrl)
+          setConfirmedSTName(characterName)
+        }
+        else if (selectMD) { 
+          setConfirmedMD(characterUrl)
+          setConfirmedMDName(characterName)
+        }
+        else if (selectDF) { 
+          setConfirmedDF(characterUrl)
+          setConfirmedDFName(characterName)
+        }
+        else if (selectAdd) { 
+          setConfirmedAdd(characterUrl)
+          setConfirmedAddName(characterName)
+        }
+        setCharacterUrl("");
+        setCharacterName("");  
+      }
+    } catch(e) {
+
+    }
+  }
+
+  
+  return (
+    <div>
+      <header style={{ minHeight: "10vh", minWidth:"100vw" }}>
+        <h1>Marvel Soccer Team</h1>
+      </header>
+      <div className="parent" style={{ minHeight: "90vh", minWidth:"100vw" }}>
+        <h2>Create your best 5-aside Marvel soccer team and share it with the world</h2>
+        <div className="selection-container">
+          <button className="selection" type="button" id="GK" onClick={OpenCharacterList}>
+            { confirmedGKName === ""? "GK"
+            : <div id="GK">
+                <div className='name' id="GK">{confirmedGKName}</div>
+                <div><CharacterImageComponent initialImageUrl={confirmedGK} id="GK"/></div>
+              </div>}
+          </button>
+          <button className="selection" type="button" id="ST" onClick={OpenCharacterList}>
+            { confirmedSTName === ""? "ST"
+            : <div id="ST">
+                <div className='name' id="ST">{confirmedSTName}</div>
+                <div><CharacterImageComponent initialImageUrl={confirmedST} id="ST"/></div>
+              </div>}
+          </button>
+          <button className="selection" type="button" id="MD" onClick={OpenCharacterList}>
+            { confirmedMDName === ""? "MD"
+            : <div id="MD">
+                <div className='name' id="MD">{confirmedMDName}</div>
+                <div><CharacterImageComponent initialImageUrl={confirmedMD} id="MD"/></div>
+              </div>}
+          </button>
+          <button className="selection" type="button" id="DF" onClick={OpenCharacterList}>
+            { confirmedDFName === ""? "DF"
+            : <div id="DF">
+              <div className='name' id="DF">{confirmedDFName}</div>
+              <div><CharacterImageComponent initialImageUrl={confirmedDF} id="DF"/></div>
+              </div>}
+          </button>
+          <button className="selection" type="button" id="Add" onClick={OpenCharacterList}>
+            { confirmedAddName === ""? "Additonal"
+            : <div id="Add">
+                <div className='name' id="Add">{confirmedAddName}</div>
+                <div><CharacterImageComponent initialImageUrl={confirmedAdd} id="Add"/></div>
+              </div>}
+          </button> 
+        </div>
+        
+        <div id="overlay">
+          <div className="search-bar-container">
+            <input
+              type="text"
+              placeholder="Search" 
+              id="keywords"
+              className="search"
+              onChange = {(e) => {setKeyWords(e.target.value)}}
+            />
+            <input type="image" className="search-button" src={searchLogo} onClick={SearchCharacter}/>
+          </div>
+          
+          <div className='characters-container'>
+            {isLoading?<div className="Loading">Loading</div>:characters}
+          </div>
+          <div className="back-logo-container"><button id="back" type="button" onClick={CloseCharacterList}><img className="back-logo" src={Back}></img></button></div>
+        </div>
+
+        <div id="overlay-2">
+          <div className="detail-container">
+            {isLoading2? <div className="Loading">Loading</div>:selected}
+            
+          </div>
+          <div className="button-container">
+            <button id="confirm" type="button" onClick={CloseDetail}>confirm</button>
+            <button id="exit-2" type="button" onClick={CloseDetail}>cancel</button>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+  );
+          
+}
